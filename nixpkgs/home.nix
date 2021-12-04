7b18aac9138b1fa6d13ca5a9f15ebafb1f1fe9b0
Deepesh Pathak <deepeshpathak09@gmail.com>
2021-12-04 11:49:08 +0000

    add home-manager config to add dotfiles
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,25 +1,41 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.gpg.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "fristonio";
   home.homeDirectory = "/home/fristonio";
 
-  home.file.".vimrc".source = "./vimrc";
-  home.file.".bashrc".source = "./bashrc";
-  home.file.".tmux.conf".source = "./tmux.conf";
-  home.file.".gitconfig".source = "./gitconfig";
-  home.file.".config/alacritty/alacritty.yml".source = "alacritty.yml";
-  home.file.".config/i3/config".source = "i3config";
-  home.file.".config/i3/locker.sh".source = "i3locker.sh";
-  home.file.".config/ls_color/ls.color".source = "ls.color";
+  home.file = {
+    ".vimrc".source = ./vimrc;
+    ".bashrc".source = ./bashrc;
+    ".tmux.conf".source = ./tmux.conf;
+    ".gitconfig".source = ./gitconfig;
+  };
+
+  xdg.configFile = {
+    "alacritty/alacritty.yml".text = builtins.readFile ./alacritty.yml;
+    "i3/config".text = builtins.readFile ./i3config;
+    "i3/locker.sh".text = builtins.readFile ./i3locker.sh;
+    "ls_color/ls.color".text = builtins.readFile ./ls.color;
+  };
 
   home.packages = [
-    pkgs.tmux
+    pkgs.bat
+    pkgs.fzf
+    pkgs.git-crypt
+    pkgs.htop
+    pkgs.jq
+    pkgs.go
+    pkgs.gopls
+    pkgs.tree
+    pkgs.watch
   ];
 
   # This value determines the Home Manager release that your
